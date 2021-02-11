b469b041c5aa4a3a1b13b2a64dd444486f81b0db
Matthias Rüedlinger <matthias.ruedlinger@spoud.io>
2021-02-11 23:12:39 +0100

    updated

--- gfollow.js
+++ gfollow.js
@@ -1,39 +1,38 @@
 const { request } = require("@octokit/request");
 require("dotenv").config();
 
-let ghToken = process.env.GH_ACCESS_TOKEM;
+function createHeader(key, value) {
+  let ghToken = process.env.GH_ACCESS_TOKEM;
+  let authHeader = {
+    headers: {
+      authorization: `token ${ghToken}`,
+    },
+  };
+  if (key) {
+    authHeader[key] = value;
+  }
+  return authHeader;
+}
 
 // get all folowers
-request("GET /user/followers", {
-  headers: {
-    authorization: `token ${ghToken}`,
-  },
-})
+request("GET /user/followers", createHeader())
   .then((resp) => {
     let followers = resp.data.map((x) => x.login);
     console.log(`\nTry to follow back all my ${followers.length} followers`);
-    resp.data.forEach((x, i) => {
-      let userFollowBack = x.login;
-      // follow user back
-      request("PUT /user/following/{username}", {
-        headers: {
-          authorization: `token ${ghToken}`,
-        },
-        username: userFollowBack,
-      }).then((resp) => {
+
+    for (let i = 0; i < followers.length; i++) {
+      let userFollowBack = followers[i];
+      request(
+        "PUT /user/following/{username}",
+        createHeader("username", userFollowBack)
+      ).then((resp) => {
         console.log(`${userFollowBack} => ${resp.status}`);
       });
-    });
+    }
     return new Set(followers);
   })
   .then((followers) => {
-    // check who is not following back
-    // console.log(followers);
-    request("GET /user/following", {
-      headers: {
-        authorization: `token ${ghToken}`,
-      },
-    }).then((resp) => {
+    request("GET /user/following", createHeader()).then((resp) => {
       let following = new Set(resp.data.map((x) => x.login));
 
       let notFollowingBack = new Set(
